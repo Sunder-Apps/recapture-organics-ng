f6182ad9d4ab6d4a09d7e786db10ce7fc80edfc8
Sam Underwood <sam@sunderapps.com>
2020-01-11 09:51:02 -0500

    Actual Contact Info

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,23 +9,13 @@ export class ContactComponent implements OnInit {
   contactInfo:any[] = [
     {
       name: 'Phone',
-      value: '555-555-5555',
+      value: '(530)-320-5555',
       icon: 'fas fa-phone'
     },
     {
       name: 'Email',
-      value: 'nathan@recaptureorganics.com',
+      value: 'recaptureorganics@gmail.com',
       icon: 'fas fa-envelope-open-text'
-    },
-    {
-      name: 'Address',
-      value: '420 High Way, CA 19023',
-      icon: 'fas fa-map-marker-alt'
-    },
-    {
-      name: 'Hours',
-      value: 'Never',
-      icon: 'fas fa-clock'
     }
   ]
   
