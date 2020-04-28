d6107ecb80516c062512893a60e21baad1dc06c6
SunderApps <sam@sunderapps.com>
2020-04-27 21:23:31 -0400

    Changed some styles

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@
 ng build --prod --base-href ./
 cp ./docs/index.html ./docs/404.html
 echo www.recaptureorganics.com > ./docs/CNAME
+rm -r ../recapture-organics-cv/www
+mkdir ../recapture-organics-cv/www
+cp ./docs/* ../recapture-organics-cv/www
+cd ../recapture-organics-cv
+cordova build --release ios
+cordova build --release android
