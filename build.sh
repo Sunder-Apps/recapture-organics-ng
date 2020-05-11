3960df265a89195edcff3856f1fb71fa3e1fbc74
SunderApps <sam@sunderapps.com>
2020-05-11 07:16:02 -0400

    New Structure

--- build.sh
+++ build.sh
@@ -11,3 +11,21 @@ cd ../recapture-organics-ng
 ng build --prod --base-href=/
 cp ./docs/index.html ./docs/404.html
 echo www.recaptureorganics.com > ./docs/CNAME
+mkdir ./docs/shop
+cp ./docs/index.html ./docs/shop/index.html
+cp ./docs/404.html ./docs/shop/404.html
+mkdir ./docs/learn
+cp ./docs/index.html ./docs/learn/index.html
+cp ./docs/404.html ./docs/learn/404.html
+mkdir ./docs/about
+cp ./docs/index.html ./docs/about/index.html
+cp ./docs/404.html ./docs/about/404.html
+mkdir ./docs/contact
+cp ./docs/index.html ./docs/contact/index.html
+cp ./docs/404.html ./docs/contact/404.html
+mkdir ./docs/privacy-policy
+cp ./docs/index.html ./docs/privacy-policy/index.html
+cp ./docs/404.html ./docs/privacy-policy/404.html
+mkdir ./docs/terms-and-conditions
+cp ./docs/index.html ./docs/terms-and-conditions/index.html
+cp ./docs/404.html ./docs/terms-and-conditions/404.html
