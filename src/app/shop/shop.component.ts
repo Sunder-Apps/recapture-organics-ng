efe63885bad31b53001a4ef3de9c5f1946793a20
SunderApps <sam@sunderapps.com>
2020-06-14 16:14:41 -0400

    Nothis

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -24,6 +24,7 @@ export class ShopComponent implements OnInit {
   }
 
   addToCart (itemID: string, variationID: string) {
+    alert('hi')
     this.shopService.setQuantity(itemID, variationID, 1)
   }
 
